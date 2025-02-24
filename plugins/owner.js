c9395fee6fe221ed635ad04fb14c71d37ecbbd8e
Irfan wahga <165075994+irfanwahga@users.noreply.github.com>
2025-02-24 22:38:00 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -29,7 +29,7 @@ async (conn, mek, m, { from }) => {
 
         // Send the owner contact message with image and audio
         await conn.sendMessage(from, {
-            image: { url: 'https://files.catbox.moe/149k8x.jpg' }, // Image URL from your request
+            image: { url: 'https://i.ibb.co/rbRfhpd/file-6273.jpg' }, // Image URL from your request
             caption: `╭━━〔 *KHAN-MD* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• *Here is the owner details*
