032863553aea401da71fbaba2c6bd26b95dc3c88
Irfan wahga <165075994+irfanwahga@users.noreply.github.com>
2025-03-04 18:06:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KHAN-MD~nzhnxQKY#vMFKWSTCOe4O1rTX4kbnG87NhMYWt71dHiiRYl5I9uY",
+SESSION_ID: process.env.SESSION_ID || "KHAN-MD~XrI3UC6Q#BVXj1z6nnNUJKClkvaLPdEduIARP8iMsNApFthiLWmA",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -61,7 +61,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923261761538",
+DEV: process.env.DEV || "923280769523",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
